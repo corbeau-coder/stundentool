064cd0062c2b48f9e64ab19fcfdbf5e2636c7cab
Corbeau-coder <joebubi@postoe.de>
2026-03-11 16:28:54 +0100

    stashing

--- src/modules/sqlite/io.py
+++ src/modules/sqlite/io.py
@@ -16,4 +16,28 @@ class ConnectionHandler:
         self.conn = sqlite3.
         return self.conn
     
-    def purge() -> 
+    def purge_db(self) -> Tuple[bool, str]:
+        logger.debug(f"IO module deleting db at {self.path}")
+        try:
+            os.remove(self.path)
+        except FileNotFoundError as e:
+            logger.debug(f"FAILED: {e}")
+            return False, str(e)
+        else:
+            logger.debug("DONE")
+            return True, ""
+        
+    self.path = path
+        if not os.path.isfile(self.path):
+            logger.debug(f"DB path is not a file {self.path}")
+            self.db_exists = False
+        else:
+            sql_string = "SELECT value FROM header"
+            try:
+                with sqlite3.connect(self.path) as conn:
+                    ret = conn.cursor().execute(sql_string).fetchone()
+                    if ret is not None:
+                        self.hours_initiated = ret
+                        self.db_exists = True
+                    else:
+                        self.db_exists = False
